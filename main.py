5c281b48f35468c4f52d5f6681eb9a565e18c103
xintong wang <xintongwang@xintongs-MacBook-Pro.local>
2018-02-07 16:41:51 -0500

    infoGain

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from pre_processor import get_samples_index
 import feature_extractor
 import numpy as np
+from sklearn.feature_selection import mutual_info_classif
 import svm_trainer
+import pdb
 
 
 if __name__ == '__main__':
-    n_levels = [5, 7, 10]
+    #n_levels = [5, 7, 10]
+    n_levels = [10]
     for n_level in n_levels:
         print("level:{}".format(n_level))
         extractor = feature_extractor.FeatureExtractor(
-            limit_order_filename="../../data/input/PN_OB_Snapshot_Aug10.xlsx",
-            feature_filename="../../data/output/features_" + str(n_level) + ".json",
+            limit_order_filename="../input/PN_OB_Snapshot_Aug10.xlsx",
+            feature_filename="../output/features_" + str(n_level) + ".json",
             time_interval=100, n_level=n_level)
         timestamps, basic_set, time_insensitive_set, labels = extractor.extract_features()
-
         sampling_index = get_samples_index(labels, num_per_label=2950)
         # selected_data = basic_set[sampling_index]
         # selected_data = time_insensitive_set[sampling_index]
         features = np.concatenate((basic_set, time_insensitive_set), axis=1)
+        #pdb.set_trace()
+        #mutual_info_classif(features, labels)
         selected_data = features[sampling_index]
         selected_labels = labels[sampling_index]
-        C = [0.5, 1.0, 1.5, 2.0, 2.5, 3]
+        C = [0.1, 1, 10, 100, 1000]
+        G = [100, 10, 1, 0.1, 0.01, 0.001, 0.0001]
         for c in C:
-            print("SVM c = {}".format(c))
-            svm_trainer.train_svm(data=selected_data, labels=selected_labels, c=c, kernel='rbf')
+            for g in G:
+                print("SVM c = {}".format(c) + " g = {}".format(g))
+                svm_trainer.train_svm(data=selected_data, labels=selected_labels, c=c, kernel='rbf', g=g)
 
--- svm_trainer.py
+++ svm_trainer.py
@@ -1,11 +1,18 @@
 from sklearn import svm
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix
+import pdb
 
 
-def train_svm(data, labels, c=1.0, kernel="rbf"):
+def train_svm(data, labels, c=1.0, kernel="rbf", g=0.01):
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=0)
-    clf = svm.SVC(C=c, kernel=kernel, gamma='auto')
+    # data preprocessing
+    #scaler = preprocessing.StandardScaler().fit(X_train)
+    #scaler.transform(X_train)
+    #scaler.transform(X_test)
+
+    clf = svm.SVC(C=c, kernel=kernel, gamma=g)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     score = clf.score(X_test, y_test)
@@ -13,4 +20,3 @@ def train_svm(data, labels, c=1.0, kernel="rbf"):
     print("SVM score is {}".format(score))
     print("confusion matrix: ")
     print(cnf_matrix)
-    print()
