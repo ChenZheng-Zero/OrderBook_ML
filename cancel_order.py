49706ea30b47be44225d3f17c97b073b3ef1c028
zerochen <zerochen@umich.edu>
2018-03-20 19:09:20 -0400

    add submission_order.py

--- cancel_order.py
+++ cancel_order.py
@@ -3,18 +3,20 @@ import numpy as np
 import os
 import pdb
 
+
 def detect_cancel(value):
     """
     Find orders that are cancelled or transacted
     """
-    length = value.shape[0] #the length of the group
+    length = value.shape[0] # the length of the group
     cancel_df = pd.DataFrame(columns=value.columns)
-    for i in range(1,length):
-        if(value.iloc[i]['SIZE']<value.iloc[i-1]['SIZE']):
+    for i in range(1, length):
+        if value.iloc[i]['SIZE'] < value.iloc[i-1]['SIZE']:
             tmp_order = value.iloc[i]
             tmp_order['SIZE'] = value.iloc[i-1]['SIZE'] - value.iloc[i]['SIZE']
             cancel_df = cancel_df.append(tmp_order)
     return cancel_df
+
 
 def merge_cancel(value):
     """
@@ -25,32 +27,31 @@ def merge_cancel(value):
     cancel_merge_df.iloc[0]['SIZE'] = value['SIZE'].sum()
     return cancel_merge_df
 
+
 def get_cancel_order(order_filename):
     """
     get cancelled orders from 'order_filename', save these orders in tgt_path
     """
     src_path = os.path.join('../PN_0816/'+order_filename)
-    tgt_path = os.path.join('../PN_0816/'+order_filename.replace('Raw','Cancel'))
-    trd_path = os.path.join('../PN_0816/'+ order_filename.replace('Order_Raw','TRD'))
-
+    tgt_path = os.path.join('../PN_0816/'+order_filename.replace('Raw', 'Cancel'))
+    trd_path = os.path.join('../PN_0816/'+order_filename.replace('Order_Raw', 'TRD'))
 
     example = pd.read_excel(src_path)
-    trade = pd.read_excel(trd_path).reindex(columns=['Time','SIZE','PRICE']).set_index(keys=['Time','PRICE'])
+    trade = pd.read_excel(trd_path).reindex(columns=['Time', 'SIZE', 'PRICE']).set_index(keys=['Time', 'PRICE'])
 
-    exm_cancel = example.groupby(['ORDER_ID'],as_index=False)
+    exm_cancel = example.groupby(['ORDER_ID'], as_index=False)
     cancel_trd = exm_cancel.apply(detect_cancel) \
-                .reset_index().drop(columns=['level_0','level_1'])
-    cancel_mrg = cancel_trd.groupby(['Time','PRICE'],as_index=False).apply(merge_cancel) \
-                .reset_index().drop(columns=['level_0','level_1'])
+                .reset_index().drop(columns=['level_0', 'level_1'])
+    cancel_mrg = cancel_trd.groupby(['Time', 'PRICE'], as_index=False).apply(merge_cancel) \
+                .reset_index().drop(columns=['level_0', 'level_1'])
 
-    cancel_ord = cancel_mrg.join(trade,on=['Time','PRICE'],rsuffix='_trd')\
-                .fillna(value={'SIZE_trd':0})
+    cancel_ord = cancel_mrg.join(trade, on=['Time','PRICE'], rsuffix='_trd')\
+                .fillna(value={'SIZE_trd': 0})
     cancel_ord['SIZE'] = cancel_ord['SIZE'] - cancel_ord['SIZE_trd']
-    cancel_ord.drop(columns='SIZE_trd',inplace=True)
-    cancel_ord = cancel_ord.loc[cancel_ord['SIZE']>0,:]
+    cancel_ord.drop(columns='SIZE_trd', inplace=True)
+    cancel_ord = cancel_ord.loc[cancel_ord['SIZE'] > 0, :]
     pdb.set_trace()
-    cancel_ord.to_excel(tgt_path,index=False)
-
+    cancel_ord.to_excel(tgt_path, index=False)
 
 
 if __name__ == '__main__':
---
+++ submission_order.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+
+def detect_submission(value):
+    """Detect submission orders."""
+    # the length of the group
+    order_dict = {}
+    length = value.shape[0]
+    submission_df = pd.DataFrame(columns=value.columns)
+    for i in range(1, length):
+        if value.iloc[i]['SIZE'] > 0:
+            if value.iloc[i]['PRICE'] not in order_dict:
+                submission_df = submission_df.append(value.iloc[i])
+            elif value.iloc[i]['SIZE'] > order_dict[value.iloc[i]['PRICE']]:
+                tmp_order = value.iloc[i]
+                tmp_order['SIZE'] = value.iloc[i]['SIZE'] - order_dict[value.iloc[i]['PRICE']]
+                submission_df = submission_df.append(tmp_order)
+        order_dict[value.iloc[i]['PRICE']] = value.iloc[i]['SIZE']
+    return submission_df
+
+
+def get_submission_order(order_filename):
+    """Get submission orders from raw data."""
+    src_path = os.path.join('../PN_0816/', order_filename)
+    tgt_path = src_path.replace('Raw', 'submission')
+
+    raw_data = pd.read_excel(src_path)
+    data = raw_data.groupby(['ORDER_ID'], as_index=False)
+    submission_ord = data.apply(detect_submission).reset_index().drop(columns=['level_0', 'level_1'])
+    submission_ord.to_excel(tgt_path, index=False)
+
+
+if __name__ == '__main__':
+    get_submission_order("PN_Order_Raw_081516.xlsx")
